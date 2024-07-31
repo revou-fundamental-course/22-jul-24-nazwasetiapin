ec178772ec480f7861022030a2cb24e0a481b63f
nazwastpn <nazwasetiapin@gmail.com>
2024-07-31 21:38:32 +0700

    update feature

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 /*Ini Javascript*/
+
+
+function formValidation() {
+    console.log('ini form');
+    let name = document.getElementById('input-name').value;
+    console.log(name);
+
+    // pengecekan dimana name tidak boleh ''
+    if (name == '') {
+        // Code ini akan di eksekusi ketika name = ''
+        alert('Maaf inputan anda kosong');
+    } else {
+        // Code ini akan di eksekusi ketika name tidak ''
+        alert('Sukses menginput');
+    }
+}
+
